d8ec45066354b5cffd23100919296d91cacbfd39
Yash Pandya <190420116045.it19@scet.ac.in>
2022-08-10 15:54:19 +0530

    add three

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+
+// import Login from "./components/Login/Login";
+// import {  useSelector } from "react-redux";
+// import { getUser, loadUser } from "./actions/user";
+// import AdminPanel from "./components/Admin/AdminPanel";
+// import Timeline from "./components/Admin/Timeline";
+// import Project from "./components/Admin/Project";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* <Route
+              path="/account"
+              element={isAuthenticated ? <AdminPanel /> : <Login />}
+            /> */}
+          {/* <Route
+              path="/admin/timeline"
+              element={isAuthenticated ? <Timeline /> : <Login />}
+            /> */}
+          {/* <Route
+              path="/admin/projects"
+              element={isAuthenticated ? <Project /> : <Login />}
+            /> */}
+        </Routes>
+        <Footer />
+      </>
+    </Router>
   );
 }
 
